73800085a2c171be4feaff0c48de765013aed2d2
Ewan Vaughan <ewan.vaughan@gmail.com>
2013-05-29 13:51:38 +1000

    Finish chapter 7 exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -24,8 +24,40 @@ describe "User pages" do
 			it "should not create a user" do
 				expect { click_button submit }.not_to change(User, :count)
 			end
-		end
+			describe "after submission" do
+				before { click_button submit }
 
+				it { should have_title full_title('Sign Up') }
+				it { should have_content('error') }
+				it { should have_content('blank') }
+				before do
+					fill_in "Name",         with: "Example User"
+					fill_in "Email",        with: "user@example.com"
+					fill_in "Password",     with: "foobar"
+				end
+				describe "where passwords don't match" do
+					before do
+						fill_in "Confirmation", with: "invalid"
+						click_button submit
+					end
+					it { should have_content "doesn't match"}
+				end
+				describe "where password is too short" do
+					before do
+						fill_in "Password", with: "short"
+						click_button submit
+					end
+					it { should have_content "too short"}
+				end
+				describe "where email is invalid" do
+					before do
+						fill_in "Email",        with: "user@invalid"
+						click_button submit
+					end
+					it { should have_content 'invalid'}
+				end
+			end
+		end
 		describe "with valid information" do
 			before do
 				fill_in "Name",         with: "Example User"
@@ -36,6 +68,14 @@ describe "User pages" do
 
 			it "should create a user" do
 				expect { click_button submit }.to change(User, :count).by(1)
+			end
+
+			describe "after saving the user" do
+				before { click_button submit }
+				let(:user) { User.find_by(email: 'user@example.com') }
+
+				it { should have_title(user.name) }
+				it { should have_selector('div.alert.alert-success', text: 'Welcome') }
 			end
 		end
 	end
